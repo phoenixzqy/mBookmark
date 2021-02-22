578f06d165353fd64aed6d0c13ab136cad1aecd3
Felix Zhao <qzhao8731@gmail.com>
2021-02-22 14:58:26 -0800

    init pwa project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import GAnalytics from 'ganalytics';
+import App from '@components/App';
+import './index.css';
+
+let root = document.querySelector('#app');
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+	let elem = root.appendChild(App());
+
+	module.hot.accept('@components/App', New => {
+		New = require('@components/App').default;
+		root.removeChild(elem);
+		elem = root.appendChild(New());
+	});
+} else if (process.env.NODE_ENV === 'production') {
+	window.ga = new GAnalytics('UA-XXXXXXXX-X');
+
+	let nxt = App();
+	let prev = root.firstElementChild;
+
+	// Check for matching DOM (`export`)
+	if (prev && prev.isEqualNode(nxt)) {
+		root.replaceChild(nxt, prev);
+	} else {
+		root.appendChild(nxt);
+	}
+
+	// Service Worker registration
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js');
+	}
+}
