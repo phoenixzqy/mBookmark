f57db09ee058640efca371a35681f624b0ad4054
Felix Zhao <felixzhao@microsoft.com>
2022-02-28 12:21:21 -0800

    add qrscanner app

--- src/components/Application/QRCodeScanner/Application.tsx
+++ src/components/Application/QRCodeScanner/Application.tsx
@@ -1,5 +1,5 @@
 export function QRCodeScannerApp(props) {
-  return <div>
-    QRCode Scanner
+  return <div style={{ width: "100%", height: "100%" }}>
+    <iframe src="https://phoenixzqy.github.io/demo/QRScanner/index.html" style={{ width: "100%", height: "100%" }} frameBorder="0" />
   </div>
 }
