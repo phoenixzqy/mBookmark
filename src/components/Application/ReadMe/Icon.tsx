82edcca48d4af13bf006707efb79c1a6e46e7ea6
Felix Zhao <felixzhao@microsoft.com>
2022-02-28 16:40:52 -0800

    fix readme

--- src/components/Application/ReadMe/Icon.tsx
+++ src/components/Application/ReadMe/Icon.tsx
@@ -4,11 +4,11 @@ import { createSignal, onMount, useContext } from 'solid-js';
 export const appid = "readme";
 
 export function Icon(props) {
-  const { launchApplication } = useContext(ApplicationManagerContext) as ApplicationManagerContextState;
+  const onclick = () => {
+    window.open('https://github.com/phoenixzqy/mBookmark#mbookmark', '_blank');
+  }
   return (
-    <div class="font-app-icon readme-app-icon" onClick={() => {
-      launchApplication(appid);
-    }}>
+    <div class="font-app-icon readme-app-icon" onClick={() => onclick()}>
       <i class="material-icons">local_library</i>
     </div>
   );
--- src/components/Application/ReadMe/index.ts
+++ src/components/Application/ReadMe/index.ts
@@ -1,13 +1,11 @@
 import { Icon, appid } from "./Icon";
 
 import { ApplicationConfig } from '../interfaces';
-import { ReadMeApp } from './Application';
 
 export const ReadmeApplication: ApplicationConfig = {
   id: appid,
   name: "ReadMe",
   description: "mBookmark user instructions",
-  icon: Icon,
-  application: ReadMeApp
+  icon: Icon
 }
 
